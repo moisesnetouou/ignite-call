e196bb8cb43632a4700ddb76e4aa9ec5e7db18dd
Moisés <moisesnetored@gmail.com>
2023-04-24 10:36:31 -0400

    feat: calendar styling

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -44,7 +44,30 @@ export function Calendar() {
             <td></td>
             <td></td>
             <td>
-              <CalendarDay>1</CalendarDay>
+              <CalendarDay disabled>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <CalendarDay>2</CalendarDay>
+            </td>{' '}
+            <td>
+              <CalendarDay>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay disabled>1</CalendarDay>
             </td>
             <td>
               <CalendarDay>2</CalendarDay>
