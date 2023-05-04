48542a7fff01505e240b863f3646e1d14e1b7ce8
Moisés <moisesnetored@gmail.com>
2023-05-04 10:12:53 -0400

    feat: loading available times

--- src/pages/schedule/[username]/ScheduleForm/CaledarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CaledarStep/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import dayjs from 'dayjs'
 import { Calendar } from '../../../../../components/Calendar'
 import {
@@ -8,16 +8,38 @@ import {
   TimePickerItem,
   TimePickerList,
 } from './styles'
+import { api } from '../../../../../lib/axios'
+import { useRouter } from 'next/router'
 
 export function CaledarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+  const [availability, setAvailability] = useState(null)
+
+  const router = useRouter()
 
   const isDateSelected = !!selectedDate
+  const username = router.query.username
 
   const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
   const describedDate = selectedDate
     ? dayjs(selectedDate).format('DD[ de ] MMMM')
     : null
+
+  useEffect(() => {
+    if (!selectedDate) {
+      return
+    }
+
+    api
+      .get(`/users/${username}/availability`, {
+        params: {
+          date: dayjs(selectedDate).format('YYYY-MM-DD'),
+        },
+      })
+      .then((response) => {
+        console.log(response.data)
+      })
+  }, [selectedDate, username])
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
